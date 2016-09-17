fe14931124e1bdcaa6f0ca01c7cf4126e1fdd5f0
Harry <hfitz83@gmail.com>
2016-09-17 21:28:54 +0100

    working by the end of the day! should work on CSS now and try deployment on real website

--- init.sql
+++ init.sql
@@ -1,3 +1,11 @@
 CREATE DATABASE UPLD;
 USE UPLD;
 
+CREATE TABLE IMAGES (
+    img_hash CHAR(32) NOT NULL,
+    img_origName VARCHAR(255) NOT NULL,
+    img_name CHAR(12) NOT NULL,
+    img_format VARCHAR(4) NOT NULL,
+    img_uplDate DATE NOT NULL
+    PRIMARY KEY (img_hash)
+);
--- upld.php
+++ upld.php
@@ -3,20 +3,26 @@
 // define the folder for the file to be saved to, and the name that it should be given
 $fileDir = "f/";
 $fileName = basename($_FILES["fileUpload"]["name"]);
-$fileExt = "." . explode(".", $fileName)[1];
-$savedFile = $fileDir . generate_name($fileName) . $fileExt;
+$fileExt = $_FILES['fileUpload']['type'];
+$fileExt = explode("/", $fileExt)[1];
+$newFileName = generate_name($fileName) . "." . $fileExt;
+$savedFile = $fileDir . $newFileName;
+
+$conn = dbConn();
 
 if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $savedFile)) {
     //if we can upload the file without any error, then upload and notify the user.
     //we use $_SERVER['SERVER_ADDR'] to make the software more portable.
     echo "file available at " . $_SERVER['SERVER_ADDR'] ."/" . $savedFile;
+    $imgHash = md5_file($savedFile);
+    insertFile($conn, $fileName, $newFileName, $fileExt, $imgHash);
+    $conn->close();
+
 }
 
-//echo "hash of file is: " . md5_file($savedFile); 
-//
 
 function generate_name($fileName) {
-    $arrName = str_split($name);
+    $arrName = str_split($fileName);
     $newFileName = '';
     $nameArr = array_merge(range('a','z'), range('A','Z'), range(0,9), $arrName);
     //while unique stuff here, important to store all filenames in database hereon.
@@ -24,5 +30,27 @@ function generate_name($fileName) {
         $newFileName .= $nameArr[array_rand($nameArr)];
     }
     return $newFileName;
+}
+
+function dbConn() {
+    //todo: move this to config file
+    $servername = "localhost";
+    $username = "root"; //yeah yeah
+    $password = ""; //naughty
+    $database = "upld";
+
+    $conn = new mysqli($servername, $username, $password, $database);
+    if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+    }
+   return $conn; 
+}
+
+function insertFile($conn, $origFileName, $newName, $ext, $hash) {
+    $curDate = date("Y-m-d");
+    $insertStatement = $conn->prepare("INSERT INTO IMAGES (img_hash,img_origName,img_name,img_format,img_uplDate) VALUES (?,?,?,?,?)");
+    $insertStatement->bind_param("sssss", $hash, $origFileName, $newName, $ext, $curDate);
+    $insertStatement->execute();
+    echo "\n\nDatabase successfully updated";
 }
 ?>
