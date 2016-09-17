4a811a9b1d8f2810652cc2c74d0090c21d2d1b2e
Harry <hfitz83@gmail.com>
2016-09-17 16:34:31 +0100

    started SQL and unique short names for files. need to validate file extensions.

--- upld.php
+++ upld.php
@@ -1,11 +1,28 @@
 <?php
 
+// define the folder for the file to be saved to, and the name that it should be given
 $fileDir = "f/";
-$savedFile = $fileDir . basename($_FILES["fileUpload"]["name"]);
-$uploadOk = 1;
+$fileName = basename($_FILES["fileUpload"]["name"]);
+$fileExt = "." . explode(".", $fileName)[1];
+$savedFile = $fileDir . generate_name($fileName) . $fileExt;
 
 if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $savedFile)) {
-    echo "file available at localhost/$savedFile";
+    //if we can upload the file without any error, then upload and notify the user.
+    //we use $_SERVER['SERVER_ADDR'] to make the software more portable.
+    echo "file available at " . $_SERVER['SERVER_ADDR'] ."/" . $savedFile;
 }
 
+//echo "hash of file is: " . md5_file($savedFile); 
+//
+
+function generate_name($fileName) {
+    $arrName = str_split($name);
+    $newFileName = '';
+    $nameArr = array_merge(range('a','z'), range('A','Z'), range(0,9), $arrName);
+    //while unique stuff here, important to store all filenames in database hereon.
+    for ( $i = 0; $i < 8; $i++ ) {
+        $newFileName .= $nameArr[array_rand($nameArr)];
+    }
+    return $newFileName;
+}
 ?>
--- upload.sh
+++ upload.sh
@@ -2,6 +2,8 @@
 
 
 if [[ -f $1 ]]; then
+    #fileUpload is the argument that PHP uses for $FILES
+    #I currently don't know if there's a standard for this.
     curl -X POST -F fileUpload=@$1 127.0.0.1:8080/upld.php
 else
     echo "Sorry, file not selected or does not exist"
