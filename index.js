f789daf07b53321037bba5fe6422bdd98e413445
Harry <hfitz83@gmail.com>
2017-04-06 22:29:40 +0100

    refactor, move to ES6

--- index.js
+++ index.js
@@ -1,122 +1,127 @@
 #!/usr/bin/env node
 
-var sqlite = require("sqlite3").verbose()
-var express = require("express");
-var multer = require("multer");
-var rg = require("rangegen");
-var fs = require("fs");
-var md5 = require("md5");
-var dbFile = "files.db";
-var db;
+const 
+    sqlite  = require("sqlite3").verbose(),
+    express = require("express"),
+    multer  = require("multer"),
+    rg      = require("rangegen"),
+    fs      = require("fs"),
+    md5     = require("md5"),
+    upld    = express(),
+    pages   = __dirname + '/pages/'
+    dbFile  = "files.db",
+    upload_dir = pages + "f/",
+    uploader = multer({
+        dest : __dirname + "/pages/f",
+        limits: {
+            files:1
+        }
+    }),
+    db
+;
 
 if (fs.existsSync(dbFile)) {
-  db = new sqlite.Database(dbFile);
+  db = new sqlite.Database(dbFile)
 } else {
-  console.error("Database file doesn't exist!");
-  process.exit(1);
+  console.error("Database file doesn't exist!")
+  process.exit(1)
 }
 
 //check if the specified table exists
-db.run("SELECT * FROM file", {}, function(err, row) {
+db.run("SELECT * FROM file", {}, (err, row) => {
   if (err) {
-    db.run("CREATE TABLE file(file_name TEXT, file_location TEXT, file_hash TEXT)", {}, function(err, row) {
+    db.run("CREATE TABLE file(file_name TEXT, file_location TEXT, file_hash TEXT)", {}, (err, row) => {
       if (err) throw err;
-    });
-
-  } else {
-    //console.log(row);
-  }
-});
+    })
+})
 
 //use express to generate an index page
-var upld = express();
-var pages = __dirname + "/pages/";
-upld.use('/', function(req,res,next) {
-  //console.log(req.method, req.url);
-  next();
-});
-upld.use('/', express.static(pages, { extensions: ['html'] }));
-upld.listen(5000);
+upld.use('/', (req,res,next) => {
+  next()
+})
 
-//handle upload
-var upload_dir = pages + "f/";
-var uploader = multer({
-  dest : __dirname + "/pages/f",
-  limits: {
-    files:1
-  }
-});
-
+upld.use('/', express.static(pages, { extensions: ['html'] }))
+upld.listen(5000)
 upld.post('/api/upld', uploader.single('upldfile'), checkExists);
 
 function checkExists(req,res) {
   //query the database for the hash
-  var hash = md5(fs.readFileSync(req.file.path));
-  console.log("file hash before upload: "+hash);
-  var query = 'SELECT file_location FROM file WHERE file_hash = "' + hash + '";';
-  db.get(query, {}, function(err,row) {
-    if (err) console.error("Error running select command: " + err);
-    //console.log("select cmd returns: "+row);
-    if (row == null) {
-      uploadFile(req,res, hash);
-    } else {
-      res.redirect(200, row.file_location);
-      //console.log(row.file_location);
-    }
-  });
-  //if it doesn't exist, upload it
+  let hash = md5(fs.readFileSync(req.file.path))
+  console.log(`file hash before upload: ${hash}`)
+  //woohoo, time to inject !
+  let query = 'SELECT file_location FROM file WHERE file_hash = "' + hash + '";'
 
-  //if it exists, return the url
-  //getFile();
-  //res.redirect("/");
+  db.get(query, {}, (err,row) => {
+
+    if (err) console.error(`Error running select command: ${err})
+
+    if (row == null) {
+      uploadFile(req,res, hash)
+    } else {
+      res.redirect(200, row.file_location)
+    }
+  })
 }
 
 function uploadFile(req,res,hash) {
-  var filename = req.file.originalname;
-  var ext = filename.split(".");
-  ext = ext[ext.length - 1]; //can't have anything on my gh without spaghetti code
+    
+  //get the filename
+  let filename = req.file.originalname
+  let ext = filename.split(".")
+  
+  //get the file extension
+  ext = ext[ext.length - 1]
+
+  //remove any whitespace
   filename = filename.replace(/\W/g, "");
-  var newName = genName(filename) + "." + ext;
+  let newName = genName(filename) + "." + ext
+  
   //move the file
-  var newArea = upload_dir + newName
-  console.log(newArea);
-  fs.rename(req.file.path, newArea, function(err) {
+  let newArea = upload_dir + newName
+  fs.rename(req.file.path, newArea, err => {
     if (err) {
-      console.error("error moving file: " + err);
+      console.error("error moving file: " + err)
     } else {
       //input to Database
-      console.log("success inserting "+newArea);
+      console.log("success inserting "+newArea)
       db.run("INSERT INTO file VALUES ($name, $location, $hash)", {
         $name: newName,
         $location: "/f/" + newName,
         $hash: md5(fs.readFileSync(newArea))
-      });
-      res.redirect(200, ("/f/"+newName));
+      })
+      res.redirect(200, ("/f/"+newName))
     }
-  });
+  })
 }
 
 //generate a filename
 function genName(name) {
-  var large = rg("a","z");
-  var smol = rg("A", "Z");
-  var nums = rg(0,9);
-  var notreallyentropy = name.split("");
-  var letts = large.concat(smol);
-  var other = nums.concat(notreallyentropy);
-  var arr = other.concat(letts);
-  var newname = "";
-  for (var i = 0; i<8;i++) {
-    var ranNum = getRandomArbitrary(0,arr.length);
+  let 
+    large = rg("a","z"),
+    smol = rg("A", "Z"),
+    nums = rg(0,9),
+    notreallyentropy = name.split(""),
+    letts = large.concat(smol),
+    other = nums.concat(notreallyentropy),
+    arr = other.concat(letts),
+    newname = ""
+  ;
+
+  for (let i = 0; i<8;i++) {
+    
+    let ranNum = getRandomArbitrary(0,arr.length)
     if (arr[ranNum - 1] != null) {
-      newname += arr[ranNum - 1];
+      newname += arr[ranNum - 1]
     }
   }
+
   return newname
 }
+
+
 //thanks mdn
 function getRandomArbitrary(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+  return Math.round(Math.random() * (max - min) + min)
 }
 
 function presentFile() {
